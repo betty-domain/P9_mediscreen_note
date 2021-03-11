b88f0601770309593713758bf88ccb4d115bede5
betty Domain <betty.domain@free.fr>
2021-03-11 15:47:23 +0100

    add list method into controller, service and repository classes

--- src/main/java/com/mediscreen/note/controller/NoteController.java
+++ src/main/java/com/mediscreen/note/controller/NoteController.java
@@ -3,11 +3,13 @@ package com.mediscreen.note.controller;
 import com.mediscreen.note.model.Note;
 import com.mediscreen.note.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class NoteController {
@@ -16,10 +18,14 @@ public class NoteController {
     private NoteService noteService;
 
     @PostMapping("/patHistory/add")
-    public Note addNote(@RequestParam Integer patientId, @RequestParam String note)
-    {
-        Note noteToCreate = new Note(patientId,note, LocalDate.now());
+    public Note addNote(@RequestParam Integer patientId, @RequestParam String note) {
+        Note noteToCreate = new Note(patientId, note, LocalDate.now());
         return noteService.addNote(noteToCreate);
+    }
+
+    @GetMapping("/patHistory")
+    public List<Note> getNotesForPatient(@RequestParam Integer patientId) {
+        return noteService.getNotesForPatient(patientId);
     }
 
 }
--- src/main/java/com/mediscreen/note/repository/NoteRepository.java
+++ src/main/java/com/mediscreen/note/repository/NoteRepository.java
@@ -3,5 +3,9 @@ package com.mediscreen.note.repository;
 import com.mediscreen.note.model.Note;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends MongoRepository<Note,String> {
+
+    List<Note> findByPatientId(Integer patientId);
 }
