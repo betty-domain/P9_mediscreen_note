fe35bfcfd603d123ce089f68c9733974d36ce3cf
betty Domain <betty.domain@free.fr>
2021-03-12 12:07:31 +0100

    add method to service ans unit tets to update Note and getOneNote by id

--- src/main/java/com/mediscreen/note/service/NoteService.java
+++ src/main/java/com/mediscreen/note/service/NoteService.java
@@ -1,5 +1,6 @@
 package com.mediscreen.note.service;
 
+import com.mediscreen.note.exceptions.NoteNotFoundException;
 import com.mediscreen.note.model.Note;
 import com.mediscreen.note.repository.NoteRepository;
 import org.apache.logging.log4j.LogManager;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NoteService {
@@ -17,16 +19,31 @@ public class NoteService {
     @Autowired
     private NoteRepository noteRepository;
 
-    public Note addNote(Note note){
+    public Note addNote(Note note) {
         logger.debug("Call to noteService.addNote");
         return noteRepository.save(note);
     }
 
-    public List<Note> getNotesForPatient(Integer patientId)
-    {
+    public List<Note> getNotesForPatient(Integer patientId) {
         logger.debug("Call to noteService.getNotesForPatient");
         return noteRepository.findByPatientId(patientId);
     }
 
+    public Note getNoteById(String noteId) throws NoteNotFoundException {
+        logger.debug("Call to noteService.getNoteById");
+        return noteRepository.findById(noteId).orElseThrow(() -> new NoteNotFoundException("Note not found"));
+    }
+
+    public Note updateNote(Note note) throws NoteNotFoundException {
+        logger.debug("Call to noteService.updateNote");
+        Optional<Note> noteToUpdate = noteRepository.findById(note.getId());
+        if (noteToUpdate.isPresent()) {
+            note.setCreatedDate(noteToUpdate.get().getCreatedDate());
+            return noteRepository.save(note);
+        } else {
+            logger.debug("updateNote : note doesn't exist");
+            throw new NoteNotFoundException("Note not found");
+        }
+    }
 
 }
--- src/test/java/com/mediscreen/note/service/NoteServiceTests.java
+++ src/test/java/com/mediscreen/note/service/NoteServiceTests.java
@@ -1,5 +1,6 @@
 package com.mediscreen.note.service;
 
+import com.mediscreen.note.exceptions.NoteNotFoundException;
 import com.mediscreen.note.model.Note;
 import com.mediscreen.note.repository.NoteRepository;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -44,5 +47,47 @@ public class NoteServiceTests {
         assertThat(noteService.getNotesForPatient(patientId).size()).isEqualTo(2);
     }
 
+    @Test
+    void updateNote_OK()
+    {
+        Note note = new Note(1,"ma prise de note de médecin", LocalDate.now());
+        when(noteRepositoryMock.findById(note.getId())).thenReturn(Optional.of(note));
+        when(noteRepositoryMock.save(note)).thenReturn(note);
 
+        assertThat(noteService.updateNote(note)).isNotNull();
+    }
+
+    @Test
+    void updatePatient_NotExist()
+    {
+        Note note = new Note(1,"ma prise de note de médecin", LocalDate.now());
+        when(noteRepositoryMock.findById(note.getId())).thenReturn(Optional.empty());
+        when(noteRepositoryMock.save(note)).thenReturn(note);
+
+        Exception exception = assertThrows(NoteNotFoundException.class, () -> {
+            noteService.updateNote(note);
+        });
+        assertThat(exception.getMessage()).contains("Note not found");
+    }
+
+    @Test
+    void getNote_OK()
+    {
+        Note note = new Note(1,"ma prise de note de médecin", LocalDate.now());
+        when(noteRepositoryMock.findById(note.getId())).thenReturn(Optional.of(note));
+
+        assertThat(noteService.getNoteById(note.getId())).isEqualTo(note);
+    }
+
+    @Test
+    void getNote_NotFound()
+    {
+        Note note = new Note(1,"ma prise de note de médecin", LocalDate.now());
+        when(noteRepositoryMock.findById(note.getId())).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(NoteNotFoundException.class, () -> {
+            noteService.getNoteById(note.getId());
+        });
+        assertThat(exception.getMessage()).contains("Note not found");
+    }
 }
