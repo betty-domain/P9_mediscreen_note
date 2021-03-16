7f815c23630ccb7b58df4359ad7188db70d043e9
betty Domain <betty.domain@free.fr>
2021-03-16 11:07:17 +0100

    test d'intrégation avec DataMongoTest

--- src/test/java/com/mediscreen/note/integration/NoteServiceIT.java
+++ src/test/java/com/mediscreen/note/integration/NoteServiceIT.java
@@ -1,20 +1,37 @@
 package com.mediscreen.note.integration;
 
 import com.mediscreen.note.model.Note;
+import com.mediscreen.note.repository.NoteRepository;
 import com.mediscreen.note.service.NoteService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@Testcontainers
+@DataMongoTest
 public class NoteServiceIT {
 
     @Autowired
     NoteService noteService;
+
+    @Autowired
+    NoteRepository noteRepository;
+
+    Note note1;
+    Note note2;
+
+    @BeforeEach
+    void insertTestData()
+    {
+        note1 = noteRepository.save(new Note(10,"Note d'un patient n°1", LocalDate.of(2010,5,10)));
+        note2 = noteRepository.save(new Note(20,"Note d'un patient n°2", LocalDate.of(2001,10,25)));
+    }
 
     @Test
     void addNote()
@@ -26,7 +43,8 @@ public class NoteServiceIT {
     @Test
     void getNotesForPatient()
     {
-        //TODO : voir avec Alexandre s'il est possible d'insérer des données avant les tests comme avec les bases MySql
-        assertThat(noteService.getNotesForPatient(1)).isNotNull();
+        assertThat(noteService.getNotesForPatient(10).size()).isEqualTo(1);
     }
+
+    //TODO add test for update and getOneNoteById
 }
